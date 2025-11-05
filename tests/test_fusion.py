dd340206ff94eba593889453b3935cf9083a13a7
mamaofthree579-ship-it <mamaofthree579@gmail.com>
2025-11-05 15:13:51 -0500

    Create test_fusion.py

---
+++ tests/test_fusion.py
@@ -0,0 +1,8 @@
+import pytest
+from src.arp.fusion import compute_S
+
+def test_compute_S_basic():
+    features = {'G':0.75, 'H':0.64, 'M':0.70, 'L':0.58, 'Ssym':0.35}
+    params = {'w':[0.18,0.26,0.18,0.22,0.16], 'theta':0.5, 'lam':6}
+    score = compute_S(features, params)
+    assert 0 <= score <= 1
