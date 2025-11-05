92f8f7c54667b0e9f4721728bcf6a78cad10e74e
mamaofthree579-ship-it <mamaofthree579@gmail.com>
2025-11-05 15:41:51 -0500

    Update test_fusion.py

--- tests/test_fusion.py
+++ tests/test_fusion.py
@@ -1,7 +1,6 @@
 import pytest
 from src.arp.fusion import compute_S
 
-
 def test_compute_S_basic():
     features = {"G": 0.75, "H": 0.64, "M": 0.70, "L": 0.58, "Ssym": 0.35}
     params = {"w": [0.18, 0.26, 0.18, 0.22, 0.16], "theta": 0.5, "lam": 6}
